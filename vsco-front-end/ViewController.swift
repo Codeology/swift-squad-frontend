18fc48d607775cf04454e2e7eec4026b742e5e0c
Kyle Hua <kyleqhua@gmail.com>
2020-10-19 20:41:00 -0700

    Initial Commit

---
+++ vsco-front-end/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vsco-front-end
+//
+//  Created by Kyle Hua on 10/19/20.
+//  Copyright © 2020 Kyle Hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ vsco-front-endTests/vsco_front_endTests.swift
@@ -0,0 +1,34 @@
+//
+//  vsco_front_endTests.swift
+//  vsco-front-endTests
+//
+//  Created by Kyle Hua on 10/19/20.
+//  Copyright © 2020 Kyle Hua. All rights reserved.
+//
+
+import XCTest
+@testable import vsco_front_end
+
+class vsco_front_endTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
