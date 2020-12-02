6699622f12419fabf8329af9b2807cdb4db78744
Kyle Hua <kyleqhua@gmail.com>
2020-12-01 22:38:10 -0800

    basic cirlces up

--- vsco-front-end/ViewController.swift
+++ vsco-front-end/ViewController.swift
@@ -10,11 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let circle1 = CAShapeLayer();
+    let circle2 = CAShapeLayer();
+    let circle3 = CAShapeLayer();
+    let circle4 = CAShapeLayer();
+    let circle5 = CAShapeLayer();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         genText()
+         drawCircles()
+        //loadImage()
     }
 
+    func drawCircles() {
+          circle1.path = UIBezierPath.init(ovalIn: CGRect(x: 65, y: 240, width: 102, height: 102)).cgPath;
+          circle1.fillColor = hexStringToUIColor(hex: "FE9B74").cgColor;
+          circle1.opacity = 1;
+          view.layer.addSublayer(circle1);
+          
+          circle2.path = UIBezierPath(ovalIn: CGRect(x: 74, y: 520, width: 79, height: 79)).cgPath;
+          view.layer.addSublayer(circle2);
+          circle2.fillColor = hexStringToUIColor(hex: "2D2428").cgColor
+          circle2.opacity = 1
+          
+          circle3.path = UIBezierPath(ovalIn: CGRect(x: 74, y: 666, width: 79, height: 79)).cgPath;
+          view.layer.addSublayer(circle3);
+          circle3.fillColor = hexStringToUIColor(hex: "E5AFA5").cgColor
+          circle3.opacity = 1
+          
+          circle4.path = UIBezierPath(ovalIn: CGRect(x: 212, y: 520, width: 79, height: 79)).cgPath;
+          view.layer.addSublayer(circle4);
+          circle4.fillColor = hexStringToUIColor(hex: "413F58").cgColor
+          circle4.opacity = 1
+          
+          circle5.path = UIBezierPath(ovalIn: CGRect(x: 212, y: 666, width: 79, height: 79)).cgPath;
+          view.layer.addSublayer(circle5);
+          circle5.fillColor = hexStringToUIColor(hex: "898198").cgColor
+          circle5.opacity = 1
+      }
+    
+    func genText() {
+        let selected = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+         selected.center = CGPoint(x: 220, y: 285)
+         selected.textAlignment = .center
+         selected.text = "Main Color"
+         
+        
+        let c1 = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        c1.center = CGPoint(x: 115, y: 500)
+        c1.textAlignment = .center
+        c1.text = "Main Color"
+        
+        let c2 = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        c2.center = CGPoint(x: 115, y: 646)
+        c2.textAlignment = .center
+        c2.text = "Main Color"
+        
+        let c3 = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        c3.center = CGPoint(x: 262, y: 500)
+        c3.textAlignment = .center
+        c3.text = "Main Color"
+        
+        let c4 = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        c4.center = CGPoint(x: 262, y: 646)
+        c4.textAlignment = .center
+        c4.text = "Main Color"
+        
+        self.view.addSubview(selected)
+        self.view.addSubview(c1)
+        self.view.addSubview(c2)
+        self.view.addSubview(c3)
+        self.view.addSubview(c4)
+    }
+    
+    func loadImage(){
+        let imageName = "yourImage.png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+        view.addSubview(imageView)
+    }
+    
+   func hexStringToUIColor (hex:String) -> UIColor {
+           var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+           if (cString.hasPrefix("#")) {
+               cString.remove(at: cString.startIndex)
+           }
+
+           if ((cString.count) != 6) {
+               return UIColor.gray
+           }
+
+           var rgbValue:UInt64 = 0
+           Scanner(string: cString).scanHexInt64(&rgbValue)
+
+           return UIColor(
+               red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+               green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+               blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+               alpha: CGFloat(1.0)
+           )
+       }
 
 }
 
